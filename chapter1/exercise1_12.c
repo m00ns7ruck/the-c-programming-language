c2b1c40531c8215de353211100609230a07b95da
m00ns7ruck <m00ns7ruck@abv.bg>
2020-06-22 13:58:38 +0300

    chapter1: Add exercise 1.12
    Signed-off-by: m00ns7ruck <m00ns7ruck@abv.bg>

---
+++ chapter1/exercise1_12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+
+    while((c = getchar()) != EOF) {
+	if((c == ' ') || (c == '\t')) {
+	    printf("\n");
+	    continue;
+	}
+
+	printf("%c", c);
+    }
+
+    return 0;
+}
